a41115353914f4b9f21ede2bfe0b65a9b282553b
Russell Jones <spam@codeofficer.com>
2009-05-25 04:58:24 -0400

    more

--- js/applications.js
+++ js/applications.js
@@ -1,6 +1,43 @@
+;(function($) {
 
 
+  var app = new Sammy.Application(function() { with(this) {
+    // this == Sammy.Application
+    
+    
+    get('#/', function() { with(this) {
+      // this == Sammy.EventContext
+      // tempate()
+      // redirect()
+      // partial()
+      $('#app').text('Hello World!')
+    }});
+  
+    // route(verb, path, callback)
+    // post()
+    // put()
+    // bind()
+    
+  }});
 
-$(function() {
-  // Stuff to do as soon as the DOM is ready;
-});
+
+  var mine = new Sammy.Application(function() { with(this) {
+    get('#/', function() { with(this) {
+      $('#mine').text('Mine!');
+    }});
+    
+    post('#/hello', function(){ with(this) {
+      // params
+      $('#mine').text(params['name']);
+      console.log(params['name']);
+    }});
+  }});
+  
+  
+  $(function() {
+    app.run('#/');
+    mine.run('#/');
+  });
+
+
+})(jQuery);
